7dc1c7b53d1bf119b9bffca97104b6af73061152
JoeDuTomato <robihuq3@gmail.com>
2017-11-26 22:52:23 -0500

    Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/DistanceNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/DistanceNavigation.java
@@ -4,7 +4,7 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.RobotMain;
+import org.firstinspires.ftc.teamcode.RobotControl;
 
 /**
  * Created by Robi on 11/1/2017.
@@ -16,7 +16,7 @@ public class DistanceNavigation {
 
 
     //Measure and replace later
-    final double rangeSensorDistanceFromEdge = RobotMain.distanceUnit.toCm(5);
+    final double rangeSensorDistanceFromEdge = RobotControl.distanceUnit.toCm(5);
     final double threshold = 20;
 
     public DistanceNavigation(HardwareMap hwMap, String floorSensorName, String wallSensorName) {
@@ -25,9 +25,9 @@ public class DistanceNavigation {
 
     }
 
-    //Gets distance in the standard unit of use in RobotMain
+    //Gets distance in the standard unit of use in RobotControl
     public double getWallDistance(){
-        return wallSensor.getDistance(RobotMain.distanceUnit) - RobotMain.distanceUnit.fromCm(rangeSensorDistanceFromEdge);
+        return wallSensor.getDistance(RobotControl.distanceUnit) - RobotControl.distanceUnit.fromCm(rangeSensorDistanceFromEdge);
     }
 
     public boolean isLineFound(){
